879e4439ccb32aaea91ca982127cfa7336a4b95a
Slava <vjacheslav.surovcev@gmail.com>
2021-11-01 12:29:35 +0300

    created a superuser, setting up the admin panel and adding filters

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+class BookInline (admin.TabularInline):
+    model = Book
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name','first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status','due_back')
+    list_display = ('book','status', 'due_back','id')
+    fieldsets = (
+        (None, {
+            'fields':('book','imprint','id')
+        }),
+        ('Avilability', {
+            'fields':('status','due_back')
+        }),
+    )
